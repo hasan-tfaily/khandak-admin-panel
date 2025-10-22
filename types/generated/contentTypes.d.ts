e647c1ce1429742756eeb177b9a584994ff2af92
hasan-tfaily <hasan.tf.11@outlook.com>
2025-10-22 22:25:31 +0300

    Add content attribute to article schema and update TypeScript definitions; format allowedTypes in edition schema for improved readability.

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -476,6 +476,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
       ['shared.media', 'shared.quote', 'shared.rich-text', 'shared.slider']
     >;
     category: Schema.Attribute.Relation<'manyToOne', 'api::category.category'>;
+    content: Schema.Attribute.RichText;
     cover: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &

eb7c322b5a0c3e00423ba0d39f92cb8208add151
hasan-tfaily <hasan.tf.11@outlook.com>
2026-03-15 01:27:25 +0200

    Refactor article lifecycle hooks to use 'publishedDate' instead of 'datePublished' and update schema to include 'publishedDate' as a date type.

--- src/api/article/content-types/article/lifecycles.ts
+++ src/api/article/content-types/article/lifecycles.ts
@@ -1,7 +1,14 @@
 export default {
   beforeCreate(event) {
-    if (!event.params.data.datePublished) {
-      event.params.data.datePublished = new Date().toISOString().split('T')[0];
+    if (!event.params.data.publishedDate) {
+      event.params.data.publishedDate = new Date().toISOString().split("T")[0];
+    }
+  },
+  beforeUpdate(event) {
+    if (!event.params.data.publishedDate && event.params.data.publishedAt) {
+      event.params.data.publishedDate = new Date(event.params.data.publishedAt)
+        .toISOString()
+        .split("T")[0];
     }
   },
 };

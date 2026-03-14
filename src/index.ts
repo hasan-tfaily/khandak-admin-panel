10d099c1c03c0e37405f5faddb501ebf9d5c2666
hasan-tfaily <hasan.tf.11@outlook.com>
2026-03-15 01:42:04 +0200

    Remove backfill migration for 'published_date' and integrate the update directly into the bootstrap function of the application.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register() {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    const knex = strapi.db.connection;
+
+    await knex.raw(`
+      UPDATE articles
+      SET published_date = published_at::date
+      WHERE published_date IS NULL AND published_at IS NOT NULL
+    `);
+  },
 };
